4fad2146fa3f30e5f48b53872c4a6d150b09926b
johns <Op871y55!>
2021-08-27 14:45:39 -0500

    modified the search to handle all types of links

--- package/sample_finder.py
+++ package/sample_finder.py
@@ -6,24 +6,33 @@ from spotipy import util
 from whosampled_scrape import *
 # from config import *
 import os
-import sys
+
+
+def parse_link(uri):
+    if 'https:' in uri:
+        uri = uri.split('/')[-1].split('?')[0]
+    if 'spotify:' in uri:
+        uri = uri.split(':')[2]
+    return uri
+
 
 def call_api(username, scope='playlist-modify-public'):
-    client='0db9481579f84081a98e19d819ecadc3'
-    secret='d8534eebbefe4b31baf794b167688a39'
+    client = '0db9481579f84081a98e19d819ecadc3'
+    secret = 'd8534eebbefe4b31baf794b167688a39'
     token = util.prompt_for_user_token(username, scope, client_id=client, client_secret=secret, redirect_uri='http://google.com/')
     return token
 
+
 def make_description(spot_dict):
-    unfound=spot_dict['unfound']
-    rate=spot_dict['rate']
+    unfound = spot_dict['unfound']
+    rate = spot_dict['rate']
     summary = 'Samples Not on Spotify: {} \nPercentage of Samples Added: {}%'.format(unfound, round(rate, 3))
     return summary
 
+
 def read_playlist(uri, sp, link=None):
-    # username = uri.split(':')[2]
     username = 1285786764
-    playlist_id = uri.split(':')[2]
+    playlist_id = parse_link(uri)
     og_tracks = []
     results = sp.user_playlist(username, playlist_id)
     for i in results['tracks']['items']:
@@ -31,10 +40,35 @@ def read_playlist(uri, sp, link=None):
         og_tracks.append({'artist' : artists, 'track':i['track']['name'].replace('Instrumental', '')})
     return og_tracks
 
+
+def read_song(uri, sp):
+    song_id = parse_link(uri)
+    og_tracks = []
+    results = sp.track(song_id)
+    og_tracks.append({'artist': results['artists'][0]['name'], 'track':results['name'].replace('Instrumental', '')})
+    return og_tracks
+
+
+def read_album(uri, sp):
+    album_id = parse_link(uri)
+    og_tracks = []
+    results = sp.album_tracks(album_id)
+    for i in results['items']:
+        artists = [j['name'] for j in i['artists']]
+        og_tracks.append({'artist': artists, 'track':i['name'].replace('Instrumental', '')})
+    return og_tracks
+
+
 def get_sample_data(uri, sp):
-    loaded_playlist = read_playlist(uri, sp)
+    if 'playlist' in uri:
+        loaded_playlist = read_playlist(uri, sp)
+    elif 'album' in uri:
+        loaded_playlist = read_album(uri, sp)
+    elif 'track' in uri:
+        loaded_playlist = read_song(uri, sp)
     new_playlist_tracks = get_whosampled_playlist(loaded_playlist)
     return new_playlist_tracks
+
 
 def get_spotify_ids(whosampled_playlist, sp):
     id_list = []
@@ -50,12 +84,10 @@ def get_spotify_ids(whosampled_playlist, sp):
                 sub_list.append(j['id'])
                 break
         if sub_list:
-#             print('FOUND ON SPOTIFY')
             id_list.append(sub_list[0])
         else:
-#             print('NO ID FOUND FOR {} by {}'.format(i['title'], artist))
             unfound_list.append((i['title']+' by '+artist))
-    location_rate=1 - len(unfound_list)/len(whosampled_playlist)
+    location_rate = 1 - len(unfound_list)/len(whosampled_playlist)
     return {'ids': id_list, 'unfound': unfound_list, 'rate': location_rate}
 
 def create_and_populate(username, new_playlist_name, spotify_dict, sp):
@@ -64,11 +96,7 @@ def create_and_populate(username, new_playlist_name, spotify_dict, sp):
     files = os.listdir('playlist_data/')
     for f in files:
         os.remove('playlist_data/'+f)
-    os.rename('samples.csv', 'playlist_data/' +playlist_id+'.csv')
-    # playlist_file  = pd.read_csv('samples.csv')
-    # print(playlist)
-    # print(playlist_id)
-    # newest_id = sp.user_playlists(username)['items'][0]['id'] #get ID of playlist just created
+    os.rename('samples.csv', 'playlist_data/' + playlist_id + '.csv')
     sp.user_playlist_add_tracks(username, playlist_id, spotify_dict['ids'], None) #populate playlist with all samples
     return playlist_id
 
@@ -87,19 +115,13 @@ def get_new_sample_playlist(uri, new_playlist_name, user):
     return new_playlist
 
 def run_program():
-    # uri = input('Please enter the Spotify URI of your playlist. \nThis can be found by clicking "Share" on your playlist and then selecting "Copy Spotify URI":\n')
-    # name = input('Please enter the name of your new sample playlist\n')
-    # username = uri.split(':')[2]
     username = 1285786764
-    # playlist_id = uri.split(':')[2]
-    # uri = "spotify:playlist:37i9dQZF1E9TtJ0IOr3OFc"
-    uri = sys.argv[1]
-    name = sys.argv[2]
+    # uri = sys.argv[1]
+    # name = sys.argv[2]
+    uri = "spotify:playlist:6uLRFki6ras4ns4VLs3dTF"
+    name = input('Please enter the name of your new sample playlist\n')
+
     new_playlist = get_new_sample_playlist(uri, name, username)
     return new_playlist
 
 run_program()
-
-#spotify:playlist:44iZ7RRjdum3DiCQiUMcN4
-#https://open.spotify.com/playlist/6uLRFki6ras4ns4VLs3dTF?si=357f592376be483e
-#spotify:playlist:6uLRFki6ras4ns4VLs3dTF
